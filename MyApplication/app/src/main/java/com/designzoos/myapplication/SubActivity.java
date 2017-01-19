930252744948882140b30e8119afbbf43ef42e80
kum ho Jeong <nashorn74@gmail.com>
2017-01-19 15:52:18 +0900

    안드로이드 스튜디오 예제

---
+++ MyApplication/app/src/main/java/com/designzoos/myapplication/SubActivity.java
@@ -0,0 +1,23 @@
+package com.designzoos.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        Button button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
